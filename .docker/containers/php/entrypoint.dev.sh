b9dafb976ae3048205752a77e75a499688798dee
SorayuthJapanya <sorayuthjaapanya@gmail.com>
2026-04-24 22:13:44 +0700

    Initial project with laravel framework.

---
+++ .docker/containers/php/entrypoint.dev.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for MySQL to be ready..."
+until php -r "new PDO('mysql:host=${DB_HOST};port=${DB_PORT};dbname=${DB_DATABASE}', '${DB_USERNAME}', '${DB_PASSWORD}');" 2>/dev/null; do
+  sleep 2
+done
+echo "MySQL is ready."
+
+php artisan migrate --force
+
+exec php artisan serve --host=0.0.0.0 --port=8000
